dc230aedd6b521e4d50223b3e5200bfe64f8d9fb
wahid9000 <wahidchowdhury9000@gmail.com>
2023-05-15 16:52:00 +0600

    api created to show total number of products on client ui

--- index.js
+++ index.js
@@ -30,6 +30,24 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const productsCollection = client.db("emaJohnDB").collection("products");
+
+
+    app.get('/products', async(req, res) => {
+        const result = await productsCollection.find().toArray();
+        res.send(result);
+    })
+
+    app.get('/totalProducts', async(req, res) => {
+        const result = await productsCollection.estimatedDocumentCount();
+        res.send({totalProducts: result})
+    })
+
+
+
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
