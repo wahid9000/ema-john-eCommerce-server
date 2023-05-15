68758f98dbf5137440e987b45767b0c78ff07917
wahid9000 <wahidchowdhury9000@gmail.com>
2023-05-15 12:30:54 +0600

    basic setup done

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+require('dotenv').config()
+const port = process.env.PORT || 5000;
+
+app.use(cors())
+app.use(express.json())
+
+app.get('/', (req, res) => {
+    res.send('ema-john-server is running')
+})
+
+app.listen(port, () => {
+    console.log(port);
+})
